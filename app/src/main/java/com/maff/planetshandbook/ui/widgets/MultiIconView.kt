7138cf0f2c7e2e15d74a6708af35eb3ccee881af
Michael Shtutman <maff.91@yandex.ru>
2017-11-27 23:47:16 +0200

    - MiltiIcon view for probe countries vis.

---
+++ app/src/main/java/com/maff/planetshandbook/ui/widgets/MultiIconView.kt
@@ -0,0 +1,73 @@
+package com.maff.planetshandbook.ui.widgets
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+
+/**
+ * Created by maff on 11/27/2017.
+ */
+
+class MultiIconView : LinearLayout {
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    private var sampleParams: ViewGroup.LayoutParams? = null
+
+    fun setImageResIds(resourceIds: Collection<Int>) {
+        if(sampleParams == null)
+        {
+            if(childCount != 0) {
+                sampleParams = getChildAt(0).layoutParams
+            }
+            else {
+                throw IllegalStateException("Place at least one image to be duplicated")
+            }
+        }
+
+        if(resourceIds.isEmpty()) {
+            removeAllViews()
+        }
+
+        removeNonImageViews()
+
+        // Fill images with resIds
+        for((index, resId) in resourceIds.withIndex())
+        {
+            if(index > (childCount - 1))
+            {
+                val view = ImageView(context)
+                view.layoutParams = sampleParams
+                view.setImageResource(resId)
+                addView(view)
+            }
+            else
+            {
+                val view = getChildAt(index) as ImageView
+                view.layoutParams = sampleParams
+                view.setImageResource(resId)
+            }
+        }
+
+        // Remove unused views`
+        if(childCount > resourceIds.size) {
+            removeViews(childCount - 1, childCount - resourceIds.size)
+        }
+    }
+
+    private fun removeNonImageViews() {
+        var i = 0
+
+        while (i < childCount)
+        {
+            if(getChildAt(i) !is ImageView) {
+                removeViewAt(i)
+                continue
+            }
+            i++
+        }
+    }
+}
